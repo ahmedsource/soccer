b892d8d677fc5d85cac66fec44fb7dc12a112f29
Ahmed Baher <abaher@roadtripnation.org>
2021-05-29 03:18:40 +0200

    initial commit, added node sass

---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,17 @@
+const Home = ({ }) => {
+  return (
+    <div className="home-page-container">
+      <div className="left-panel">
+
+      </div>
+      <div className="middle-panel">
+
+      </div>
+      <div className="right-panel">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Home from './containers/Home';
+import './scss/main.scss';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
